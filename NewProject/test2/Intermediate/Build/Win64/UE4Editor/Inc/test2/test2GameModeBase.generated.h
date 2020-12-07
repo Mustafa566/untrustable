3bb4d4a0b1ae6376b982e5210612fc108b4f3ca0
JoeyMalta <Joeypmalta@gmail.com>
2020-12-07 19:22:52 +0100

    Update

---
+++ NewProject/test2/Intermediate/Build/Win64/UE4Editor/Inc/test2/test2GameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TEST2_test2GameModeBase_generated_h
+#error "test2GameModeBase.generated.h already included, missing '#pragma once' in test2GameModeBase.h"
+#endif
+#define TEST2_test2GameModeBase_generated_h
+
+#define test2_Source_test2_test2GameModeBase_h_15_SPARSE_DATA
+#define test2_Source_test2_test2GameModeBase_h_15_RPC_WRAPPERS
+#define test2_Source_test2_test2GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define test2_Source_test2_test2GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAtest2GameModeBase(); \
+	friend struct Z_Construct_UClass_Atest2GameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(Atest2GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/test2"), NO_API) \
+	DECLARE_SERIALIZER(Atest2GameModeBase)
+
+
+#define test2_Source_test2_test2GameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAtest2GameModeBase(); \
+	friend struct Z_Construct_UClass_Atest2GameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(Atest2GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/test2"), NO_API) \
+	DECLARE_SERIALIZER(Atest2GameModeBase)
+
+
+#define test2_Source_test2_test2GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API Atest2GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(Atest2GameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, Atest2GameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(Atest2GameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API Atest2GameModeBase(Atest2GameModeBase&&); \
+	NO_API Atest2GameModeBase(const Atest2GameModeBase&); \
+public:
+
+
+#define test2_Source_test2_test2GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API Atest2GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API Atest2GameModeBase(Atest2GameModeBase&&); \
+	NO_API Atest2GameModeBase(const Atest2GameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, Atest2GameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(Atest2GameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(Atest2GameModeBase)
+
+
+#define test2_Source_test2_test2GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define test2_Source_test2_test2GameModeBase_h_12_PROLOG
+#define test2_Source_test2_test2GameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	test2_Source_test2_test2GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	test2_Source_test2_test2GameModeBase_h_15_SPARSE_DATA \
+	test2_Source_test2_test2GameModeBase_h_15_RPC_WRAPPERS \
+	test2_Source_test2_test2GameModeBase_h_15_INCLASS \
+	test2_Source_test2_test2GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define test2_Source_test2_test2GameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	test2_Source_test2_test2GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	test2_Source_test2_test2GameModeBase_h_15_SPARSE_DATA \
+	test2_Source_test2_test2GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	test2_Source_test2_test2GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	test2_Source_test2_test2GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TEST2_API UClass* StaticClass<class Atest2GameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID test2_Source_test2_test2GameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ NewProject/test2/Intermediate/Build/Win64/test2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "Z:/Untrustable/untrustables/NewProject/test2/Intermediate/Build/Win64/test2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
